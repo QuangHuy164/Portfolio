6d840e7b5d297a7700c0791703238f80f37a2663
QuangHuy164 <nghuy164@gmail.com>
2024-12-07 05:36:46 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Radio from '../models/Radio'
 
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"></div> */}
 
@@ -10,11 +11,14 @@ const Home = () => {
       <Canvas className="w-full h-fullscreen bg-transparent"
               camera={{ near: 0.1, far: 1000}}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
           <directionalLight />
           <ambientLight />
           <pointLight />
           <spotLight />
+          <hemisphereLight />
+          <Radio />
+          </Suspense>
       </Canvas>
     </section>
   );
