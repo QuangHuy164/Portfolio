031f6e2654b97d92c87e4b6c942b3ce79d464b04
QuangHuy164 <nghuy164@gmail.com>
2025-06-29 05:12:20 +0300

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+import emailjs from '@emailjs/browser'
 const Contact = () => {
+  const formRef = useRef(null)
   const [form, setForm] = useState({name:'', email:'', message:''})
-  const handleChange = () => {}
-  const handleFocus = () => {}
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name] : e.target.value })
+  }
+  const handleFocus = (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    emailjs.sendForm()
+  }
   const handleBlur = () => {}
+  const handleSubmit = () => {}
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
         <form className="w-full flex flex-col gap-7 mt-14"
+              onSubmit={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
@@ -53,7 +65,8 @@ const Contact = () => {
               onBlur={handleBlur}
             />
           </label>
-          <button type="submit" className="btn" onFocus={handleFocus} onBlur={handleBlur}>
+          <button type="submit" className="btn" disabled= {isLoading} onFocus={handleFocus} onBlur={handleBlur}>
+            {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
       </div>
