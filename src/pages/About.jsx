2667d4ee6d24c76a93f8be95674e78cafc6d3635
QuangHuy164 <nghuy164@gmail.com>
2025-07-04 02:52:33 +0300

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,38 @@
-import { skills } from "../constants"
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { skills, experiences } from "../constants";
 
 const About = () => {
   return (
     <section className="max-container">
       <h1 className="head-text">
-        Hello, I am <span className="blue-gradient_text font-semibold drop-shadow">Huy</span>
+        Hello, I am{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Huy
+        </span>
       </h1>
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
-        <p>Software Engineer based in Helsinki, trying to find an opportunity in a Software Development role and a good company, specializing in technical education through hands-on learning
-          and learning applications. I just graduated from university and am open to work. I have fundamental skills in front-end development and the basics of NodeJS. </p>
+        <p>
+          Software Engineer based in Helsinki, trying to find an opportunity in
+          a Software Development role and a good company, specializing in
+          technical education through hands-on learning and learning
+          applications. I just graduated from university and am open to work. I
+          have fundamental skills in front-end development and the basics of
+          NodeJS.{" "}
+        </p>
       </div>
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
 
         <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill)=> (
+          {skills.map((skill) => (
             <div className="block-container w-20 h-20">
-              <div className="btn-back rounded-xl"/>
+              <div className="btn-back rounded-xl" />
               <div className="btn-front rounded-xl flex justify-center items-center">
-                <img 
+                <img
                   src={skill.imageUrl}
                   alt={skill.name}
                   className="w-1/2 h-1/2 object-contain"
@@ -31,12 +45,45 @@ const About = () => {
       <div className="py-16">
         <h3 className="subhead-text">Work Experience</h3>
         <div className="mt-5 flex flex-col gap-3 text-slate-500">
-        <p>In my work experience section, 
-        </p>
+          <p>
+            In my work experience section, I got an internship in Darkglass
+            Electronics Oy base in Helsinki, Finland. During the internship
+            time, I leveled up my coding skills and met several experts in
+            development field. Here's the rundown:
+          </p>
+        </div>
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement key={experience.company_name} date={experience.date}>
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {experience.title}
+                  </h3>
+                  <p
+                    className="text-black-500 font-medium font-base"
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
       </div>
-      </div>
-      </section>
-  )
-}
+    </section>
+  );
+};
 
-export default About
+export default About;
